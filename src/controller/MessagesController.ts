c83040969f9e4c7481ae6c5086a5161b1b85b6d7
julioceno <julioreplay@gmail.com>
2021-04-22 17:00:48 -0300

    aula 4 pronta

--- src/controller/MessagesController.ts
+++ src/controller/MessagesController.ts
@@ -1,5 +1,5 @@
 import { Request, Response} from "express"
-import { MessagesService } from "../services/MessageService"
+import { MessagesService } from "../services/MessagesService"
 
 class MessagesController {
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
-import express, { json } from "express";
+import { http } from "./http";
+import "./websockets/client";
 
-import "./database" ; // Como o arquivo que quero importar é o index não preciso especificar isso no import
-import { routes } from "./routes";
+http.listen(3333, () => console.log('Server is running on port 3333'));
 
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333, () => console.log('Server is running on port 3333'));
